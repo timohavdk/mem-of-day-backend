c2e3cb732eb5c669f877fe268ffe4664f1df7474
Timofey Andreev <andreev.ta1@dns-shop.ru>
2023-12-12 16:56:03 +1000

    Controller

--- controllers/imageControllers.js
+++ controllers/imageControllers.js
@@ -1,5 +1,10 @@
-const generateController = (request, response) => {
-    
+import { compositeWordInImage } from "../lib/composite.js";
+
+const generateController = async (request, response) => {
+    const bufferImage = await compositeWordInImage();
+
+    response.set('Content-Type', 'image/jpeg');
+    response.send(bufferImage);
 }
 
 export const imageControllers = {
